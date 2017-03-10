7368db047b179935fadef1ae1b3df0bef550546c
Abenezer yakob <abenezer.yakob@gmail.com>
2017-03-10 23:18:05 +0100

    Arduino car warmer through GSM network

---
+++ arduinorele/arduinorele.ino
@@ -0,0 +1,352 @@
+/***************************************************
+  This is an example for our Adafruit FONA Cellular Module
+
+  Designed specifically to work with the Adafruit FONA
+  ----> http://www.adafruit.com/products/1946
+  ----> http://www.adafruit.com/products/1963
+  ----> http://www.adafruit.com/products/2468
+  ----> http://www.adafruit.com/products/2542
+
+  These cellular modules use TTL Serial to communicate, 2 pins are
+  required to interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+/*
+THIS CODE IS STILL IN PROGRESS!
+
+Open up the serial console on the Arduino at 115200 baud to interact with FONA
+
+Note that if you need to set a GPRS APN, username, and password scroll down to
+the commented section below at the end of the setup() function.
+*/
+#include "Adafruit_FONA.h"
+
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+
+// this is a large buffer for replies
+char replybuffer[255];
+
+// We default to using software serial. If you want to use hardware serial
+// (because softserial isnt supported) comment out the following three lines 
+// and uncomment the HardwareSerial line
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+// Hardware serial is also possible!
+//  HardwareSerial *fonaSerial = &Serial1;
+
+// Use this for FONA 800 and 808s
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+// Use this one for FONA 3G
+//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+
+uint8_t type;
+int relPin = 8;
+
+void setup() {
+
+  pinMode(relPin,OUTPUT);
+  digitalWrite(relPin,LOW);
+  while (!Serial);
+
+  Serial.begin(115200);
+  //Serial.println(F("FONA basic test"));
+  //Serial.println(F("Initializing....(May take 3 seconds)"));
+
+  fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while (1);
+  }
+  type = fona.type();
+  Serial.println(F("FONA is OK"));
+  Serial.print(F("Found "));
+  switch (type) {
+    case FONA800L:
+      Serial.println(F("FONA 800L")); break;
+    case FONA800H:
+      Serial.println(F("FONA 800H")); break;
+    case FONA808_V1:
+      Serial.println(F("FONA 808 (v1)")); break;
+    case FONA808_V2:
+      Serial.println(F("FONA 808 (v2)")); break;
+    case FONA3G_A:
+      Serial.println(F("FONA 3G (American)")); break;
+    case FONA3G_E:
+      Serial.println(F("FONA 3G (European)")); break;
+    default: 
+      Serial.println(F("???")); break;
+  }
+  
+  // Print module IMEI number.
+  char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+
+  // Optionally configure a GPRS APN, username, and password.
+  // You might need to do this to access your network's GPRS/data
+  // network.  Contact your provider for the exact APN, username,
+  // and password values.  Username and password are optional and
+  // can be removed, but APN is required.
+  //fona.setGPRSNetworkSettings(F("your APN"), F("your username"), F("your password"));
+
+  // Optionally configure HTTP gets to follow redirects over SSL.
+  // Default is not to follow SSL redirects, however if you uncomment
+  // the following line then redirects over SSL will be followed.
+  //fona.setHTTPSRedirect(true);
+
+  printMenu();
+}
+
+void printMenu(void) {
+  Serial.println(F("-------------------------------------"));
+ 
+
+
+}
+void loop() {
+  //Serial.print(F("FONA> "));
+ 
+
+  char command = 'r';
+  //Serial.println(command);
+
+
+  switch (command) {
+    
+    case 'r': {
+        // read an SMS
+        flushSerial();
+        //Serial.print(F("Read #"));
+        uint8_t smsn = 1;
+        //Serial.print(F("\n\rReading SMS #")); Serial.println(smsn);
+
+        // Retrieve SMS sender address/phone number.
+        if (! fona.getSMSSender(smsn, replybuffer, 250)) {
+          //Serial.println("Failed!");
+          break;
+        }
+        //Serial.print(F("FROM: ")); Serial.println(replybuffer);
+
+        // Retrieve SMS value.
+        uint16_t smslen;
+        if (! fona.readSMS(smsn, replybuffer, 250, &smslen)) { // pass in buffer and max len!
+          //Serial.println("Failed!");
+          break;
+        }
+        //Serial.println(replybuffer);
+        //Serial.println("                                  lalalalal                           ");
+        if (String(replybuffer)=="On"){
+          //Serial.println(replybuffer);
+          //Serial.println(F("******************************************************************"));
+          
+          pinMode(relPin,OUTPUT);
+          digitalWrite(relPin,HIGH);
+          //Serial.println(replybuffer);
+          //Serial.println(replybuffer);
+           //uint8_t smsn = readnumber();
+
+
+           //Serial.print(F("\n\rDeleting SMS #")); Serial.println(1);
+        if (fona.deleteSMS(1)) {
+          //Serial.println(F("OK!lalalallalala"));
+          break;
+         
+        } else {
+          Serial.println(F("Couldn't delete"));
+        }
+
+           
+          }
+
+
+
+          else if (String(replybuffer)=="Off"){
+          //Serial.println(replybuffer);
+          //Serial.println(F("******************************************************************"));
+          
+          pinMode(relPin,OUTPUT);
+          digitalWrite(relPin,LOW);
+          //Serial.println(replybuffer);
+          //Serial.println(replybuffer);
+           
+
+
+           //Serial.print(F("\n\rDeleting SMS #")); Serial.println(1);
+        if (fona.deleteSMS(1)) {
+          //Serial.println(F("OK!lalalallalala"));
+          break;
+         
+        } else {
+          Serial.println(F("Couldn't delete"));
+        }
+
+           
+          }
+
+
+   
+
+           // delete an SMS
+        
+        //Serial.print(F("Delete #"));
+        //uint8_t smsnn = readnumber();
+
+        Serial.print(F("\n\rDeleting SMS #")); Serial.println(1);
+        if (fona.deleteSMS(1)) {
+          Serial.println(F("OK!"));
+          pinMode(relPin,OUTPUT);
+          digitalWrite(relPin,LOW);
+        } else {
+          Serial.println(F("Couldn't delete"));
+        }
+        
+
+        break;
+      }
+    
+
+    case 'd': {
+        // delete an SMS
+        flushSerial();
+        Serial.print(F("Delete #"));
+        uint8_t smsn = readnumber();
+
+        Serial.print(F("\n\rDeleting SMS #")); Serial.println(smsn);
+        if (fona.deleteSMS(smsn)) {
+          Serial.println(F("OK!"));
+        } else {
+          Serial.println(F("Couldn't delete"));
+        }
+        break;
+      }
+
+
+
+
+
+
+    case 't': {
+        // read the time
+        char buffer[23];
+
+        fona.getTime(buffer, 23);  // make sure replybuffer is at least 23 bytes!
+        Serial.print(F("Time = ")); Serial.println(buffer);
+        break;
+      }
+
+
+    /*********************************** GPS (SIM808 only) */
+
+
+    /*********************************** GPRS */
+
+    /*****************************************/
+
+    case 'S': {
+        Serial.println(F("Creating SERIAL TUBE"));
+        while (1) {
+          while (Serial.available()) {
+            delay(1);
+            fona.write(Serial.read());
+          }
+          if (fona.available()) {
+            Serial.write(fona.read());
+          }
+        }
+        break;
+      }
+
+    default: {
+        Serial.println(F("Unknown command"));
+        printMenu();
+        break;
+      }
+  }
+
+
+  
+  // flush input
+  flushSerial();
+  while (fona.available()) {
+    Serial.write(fona.read());
+  }
+
+}
+
+void flushSerial() {
+  while (Serial.available())
+    Serial.read();
+}
+
+char readBlocking() {
+  while (!Serial.available());
+  return Serial.read();
+}
+uint16_t readnumber() {
+  uint16_t x = 0;
+  char c;
+  while (! isdigit(c = readBlocking())) {
+    //Serial.print(c);
+  }
+  Serial.print(c);
+  x = c - '0';
+  while (isdigit(c = readBlocking())) {
+    Serial.print(c);
+    x *= 10;
+    x += c - '0';
+  }
+  return x;
+}
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout) {
+  uint16_t buffidx = 0;
+  boolean timeoutvalid = true;
+  if (timeout == 0) timeoutvalid = false;
+
+  while (true) {
+    if (buffidx > maxbuff) {
+      //Serial.println(F("SPACE"));
+      break;
+    }
+
+    while (Serial.available()) {
+      char c =  Serial.read();
+
+      //Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+
+      if (c == '\r') continue;
+      if (c == 0xA) {
+        if (buffidx == 0)   // the first 0x0A is ignored
+          continue;
+
+        timeout = 0;         // the second 0x0A is the end of the line
+        timeoutvalid = true;
+        break;
+      }
+      buff[buffidx] = c;
+      buffidx++;
+    }
+
+    if (timeoutvalid && timeout == 0) {
+      //Serial.println(F("TIMEOUT"));
+      break;
+    }
+    delay(1);
+  }
+  buff[buffidx] = 0;  // null term
+  return buffidx;
+}
